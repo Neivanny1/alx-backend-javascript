93f48e763869b390ce1a4de857d55c1bab0759f0
Neivanny1 <nevileneivanny@gmail.com>
2024-03-04 17:11:44 +0000

    remove unnecessary code comments

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
   
     if (trueOrFalse) {
-      const task = true; // eslint-disable-line no-unused-vars
-      const task2 = false; // eslint-disable-line no-unused-vars
+      const task = true;
+      const task2 = false;
     }
   
     return [task, task2];
