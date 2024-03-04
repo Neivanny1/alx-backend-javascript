5b4dc2a673c0fa573e4e994ea7383b5332d782bc
Neivanny1 <nevileneivanny@gmail.com>
2024-03-04 17:46:43 +0000

    added indentation to match eslint

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,11 @@
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+    const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
-    const self = this;
-    this.addNeighborhood = (newNeighborhood) => {
-      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
+    const addNeighborhood = (newNeighborhood) => {
+      sanFranciscoNeighborhoods.push(newNeighborhood);
+      return sanFranciscoNeighborhoods;
     };
+  
+    return [sanFranciscoNeighborhoods, addNeighborhood];
   }
+  
