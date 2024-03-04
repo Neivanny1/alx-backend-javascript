9626c285de4b49b668f5968601f5e0d9c57ce370
Neivanny1 <nevileneivanny@gmail.com>
2024-03-04 19:58:07 +0000

    added indentation to match eslint

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -3,6 +3,6 @@ export default function iterateThroughObject(reportWithIterator) {
   for (const employee of reportWithIterator) {
     employees.push(employee);
   }
-  
+
   return employees.join(' | ');
 }
