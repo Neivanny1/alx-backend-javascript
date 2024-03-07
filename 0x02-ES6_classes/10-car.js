2a0abc4905cc83300ed3d98e6d07ae504229d868
Neivanny1 <nevileneivanny@gmail.com>
2024-03-07 18:45:45 +0000

    fixed error from lint about styling standards

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,14 +1,16 @@
-import Car from './10-car';
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-class TestCar extends Car {}
-
-const tc1 = new TestCar('Nissan', 'Turbo', 'Pink');
-const tc2 = tc1.cloneCar();
-
-console.log(tc1);
-console.log(tc1 instanceof TestCar);
-
-console.log(tc2);
-console.log(tc2 instanceof TestCar);
-
-console.log(tc1 === tc2);
+  cloneCar() {
+    const current = this;
+    return Object.assign(Object.create(Object.getPrototypeOf(current)), {
+      _brand: undefined,
+      _motor: undefined,
+      _color: undefined,
+    });
+  }
+}
