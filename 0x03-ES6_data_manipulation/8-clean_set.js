b509699a4f6f4f4ca189762572cdaa88ed7ea07f
Neivanny1 <nevileneivanny@gmail.com>
2024-03-12 01:40:27 +0000

    returns a string of all the set values that start with a specific string (startString).

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,11 @@
-export default function cleanSet(set, startString) {
-  const filteredValues = Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.substring(startString.length));
-  return filteredValues.join('-');
+export default function cleanSet(_set, startString) {
+  if (typeof _set !== 'object') return '';
+  if (typeof startString !== 'string') return '';
+  if (startString.length === 0) return '';
+
+  const stringSet = [];
+  [..._set].forEach((x) => {
+    if (x && x.indexOf(startString) === 0) stringSet.push(x.replace(startString, ''));
+  });
+  return stringSet.join('-');
 }
