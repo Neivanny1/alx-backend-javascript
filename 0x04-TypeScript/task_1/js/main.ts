79b7a4556c0cc8abc03fc843d282d7584c31eed7
Neivanny1 <nevileneivanny@gmail.com>
2024-03-13 09:53:15 +0000

    added Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,8 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any;
+}
