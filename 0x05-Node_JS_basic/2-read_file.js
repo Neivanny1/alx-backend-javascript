201a48eb57137de51a70b728b7b4ce52e7cca53b
Neivanny1 <nevileneivanny@gmail.com>
2024-04-29 21:58:09 +0000

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,12 +7,13 @@ const fs = require('fs');
 module.exports = function countStudents (path) {
   try {
     const data = fs.readFileSync(path, { encoding: 'utf-8' });
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-    const header = lines.shift().split(',');
+    const lines = data.split('\n').slice(1, -1);
+    const header = data.split('\n').slice(0, 1)[0].split(',');
     const idxFn = header.findIndex((ele) => ele === 'firstname');
     const idxFd = header.findIndex((ele) => ele === 'field');
     const fields = {};
     const students = {};
+
     lines.forEach((line) => {
       const list = line.split(',');
       if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
@@ -20,6 +21,7 @@ module.exports = function countStudents (path) {
       if (!students[list[idxFd]]) students[list[idxFd]] = '';
       students[list[idxFd]] += students[list[idxFd]] ? `, ${list[idxFn]}` : list[idxFn];
     });
+
     console.log(`Number of students: ${lines.length}`);
     for (const key in fields) {
       if (Object.hasOwnProperty.call(fields, key)) {
