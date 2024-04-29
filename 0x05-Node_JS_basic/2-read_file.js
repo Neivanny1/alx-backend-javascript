7e63c1106049af9d48d0034c56a197315acb5b71
Neivanny1 <nevileneivanny@gmail.com>
2024-04-29 21:41:07 +0000

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,29 +7,42 @@ const fs = require('fs');
 module.exports = function countStudents (path) {
   try {
     const data = fs.readFileSync(path, { encoding: 'utf-8' });
-    const lines = data.split('\n').slice(1, -1);
-    const header = data.split('\n').slice(0, 1)[0].split(',');
-    const idxFn = header.findIndex((ele) => ele === 'firstname');
-    const idxFd = header.findIndex((ele) => ele === 'field');
+    const lines = data.trim().split('\n');
+
+    if (lines.length === 0) {
+      console.log('Number of students: 0');
+      return;
+    }
+
+    const header = lines[0].split(',');
+    const idxFn = header.indexOf('firstname');
+    const idxFd = header.indexOf('field');
     const fields = {};
     const students = {};
 
-    lines.forEach((line) => {
+    lines.slice(1).forEach(line => {
       const list = line.split(',');
-      if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
-      fields[list[idxFd]] += 1;
-      if (!students[list[idxFd]]) students[list[idxFd]] = '';
-      students[list[idxFd]] += students[list[idxFd]] ? `, ${list[idxFn]}` : list[idxFn];
+      if (list.length >= 2) {
+        const field = list[idxFd];
+        const firstName = list[idxFn];
+
+        if (!fields[field]) fields[field] = 0;
+        fields[field]++;
+
+        if (!students[field]) students[field] = [];
+        students[field].push(firstName);
+      }
     });
 
-    console.log(`Number of students: ${lines.length}`);
+    console.log(`Number of students: ${lines.length - 1}`);
     for (const key in fields) {
       if (Object.hasOwnProperty.call(fields, key)) {
         const element = fields[key];
-        console.log(`Number of students in ${key}: ${element}. List: ${students[key]}`);
+        console.log(`Number of students in ${key}: ${element}. List: ${students[key].join(', ')}`);
       }
     }
   } catch (error) {
-    throw new Error('Cannot load the database');
+    console.error('Cannot load the database');
+    process.exit(1);
   }
 };
