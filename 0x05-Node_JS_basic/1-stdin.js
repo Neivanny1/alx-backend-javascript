4ba816c65911fe096b9267b50c174370367432a4
Neivanny1 <nevileneivanny@gmail.com>
2024-04-29 21:18:35 +0000

    fixed styling issue

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,9 @@
 //  program named 1-stdin.js that will be executed through command line
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  process.stdout.write(`Your name is: ${input}`);
+  if (input !== null) process.stdout.write(`Your name is: ${input}`);
+});
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
