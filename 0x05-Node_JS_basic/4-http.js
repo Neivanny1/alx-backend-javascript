5d6e24bab28aabc97b8e7a753fa5839ee6e7c6b3
Neivanny1 <nevileneivanny@gmail.com>
2024-04-30 12:27:04 +0000

    creating a small HTTP server using the http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+// creating a small HTTP server using the http module
+const http = require('http');
+const host = '127.0.0.1';
+const port = 1245;
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, host);
+
+module.exports = app;
