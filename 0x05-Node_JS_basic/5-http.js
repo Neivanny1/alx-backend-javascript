3be75e48ac8116c48c9aacf74fc41804494c04bc
Neivanny1 <nevileneivanny@gmail.com>
2024-04-30 12:57:58 +0000

    small HTTP server using Express

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -6,17 +6,12 @@ function countStudents (path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
       if (err) return reject(Error('Cannot load the database'));
-      // split data and taking only list without header
       const lines = data.split('\n').slice(1, -1);
-      // give the header of data
       const header = data.split('\n').slice(0, 1)[0].split(',');
-      // find firstname and field index
       const idxFn = header.findIndex((ele) => ele === 'firstname');
       const idxFd = header.findIndex((ele) => ele === 'field');
-      // declarate two dictionaries for count each fields and store list of students
       const fields = {};
       const students = {};
-      // it will contain all data
       const all = {};
 
       lines.forEach((line) => {
