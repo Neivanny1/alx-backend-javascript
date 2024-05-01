fded10040c4252d1a28cafc1da23f1d311325794
Neivanny1 <nevileneivanny@gmail.com>
2024-05-01 13:13:07 +0000

    fixed lint issues

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,10 +4,10 @@
 // If the database is not available, it should print an error message and exit
 const fs = require('fs');
 
-module.exports = function countStudents (path) {
+module.exports = function countStudents(path) {
   try {
     const data = fs.readFileSync(path, { encoding: 'utf-8' });
-    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
     const header = lines.shift().split(',');
     const idxFn = header.findIndex((ele) => ele === 'firstname');
     const idxFd = header.findIndex((ele) => ele === 'field');
