bdd942fec8592630a2d1c009dd2ee54fa5d2563b
Neivanny1 <nevileneivanny@gmail.com>
2024-05-01 14:12:56 +0000

    Organize a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,8 +1,7 @@
 class AppController {
-    static getHomepage(request, response) {
-      response.status(200).send('Hello Holberton School!');
-    }
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
-  
-  module.exports = AppController;
-  
+}
+
+module.exports = AppController;
